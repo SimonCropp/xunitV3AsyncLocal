79f5e1dd046fd036037fd9c9d1af71ac1d31aacc
Simon Cropp <simon.cropp@gmail.com>
2024-09-25 18:57:38 +1000

    .

---
+++ V3/Tests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+public class Tests
+{
+    private AsyncLocal<object> asyncLocal = new();
+
+    public Tests()
+    {
+        asyncLocal.Value = new();
+    }
+
+    [Fact]
+    public void Test()
+    {
+        Assert.NotNull(asyncLocal.Value);
+    }
+}
